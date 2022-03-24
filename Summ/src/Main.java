8322c32c5755c4a0480dbc17fee714343e8ba6cb
Ferrara1989 <89211024+Ferrara1989@users.noreply.github.com>
2022-03-24 20:27:41 +0300

    class Wheel

--- Summ/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Arithmetic arithmetic = new Arithmetic(5, 10);
-        arithmetic.sum();
-    }
-}
---
+++ new project/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Wheel tur = new Wheel(3 );
+        tur.setNumberOfWheels(6);
+        System.out.println(tur.getNumberOfWheels());
+    }
+}
